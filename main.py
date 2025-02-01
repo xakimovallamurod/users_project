cba132b909fa40a01892d47177764472633e459a
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2025-02-01 19:09:36 +0200

    main file

---
+++ main.py
@@ -0,0 +1,73 @@
+import csv
+import json
+
+def read_csv(file_path):
+    # Read and parse the CSV file
+        
+    # Return a readDict object
+
+    return 
+
+def csv_to_json(file_path, data):
+    # Convert all data to a dictionary
+
+    # write data to JSON file
+
+    return
+
+def json_data_all(file_path):
+    # JSON file loads 
+
+    # return all users data
+
+    return
+
+def first_name_all(data):
+
+    # Users data parse
+
+    # retrun first_name all list type
+    
+    return
+
+def last_name_all(data):
+
+    # Users data parse
+
+    # retrun last_name all list type
+    
+    return
+
+def gender_male_count(data):
+
+    # Users data parse
+
+    # retrun gender mael count 
+    
+    return
+
+def gender_female_count(data):
+
+    # Users data parse
+
+    # retrun gender femael count 
+    
+    return
+
+def get_all_domen_name(data):
+
+    # Users data parse
+
+    # Return all unique domain names from the data.
+
+    # sample: username@gmail.com -> domen_name = 'gmail'
+
+    return
+
+def get_job(data, job):
+
+    # Get user data where the job is equal to the specified job
+
+    # Return a list of entries with the same job
+
+    return
